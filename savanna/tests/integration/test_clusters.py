3848ef69620d9f7c1c3939a57f419c8f97d1af85
LobankovYV <lobankovyy@lobankovyy-desktop.(none)>
2013-04-30 21:50:15 +0400

    run_fast_checks

--- savanna/tests/integration/test_clusters.py
+++ savanna/tests/integration/test_clusters.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#import eventlet
 from savanna.tests.integration.db import ValidationTestCase
 from telnetlib import Telnet
 
@@ -23,19 +24,37 @@ class TestValidationApiForClusters(ValidationTestCase):
         super(TestValidationApiForClusters, self).setUp()
         Telnet(self.host, self.port)
 
-    # def test_crud_operation_for_cluster(self):
-    #     nt_body = self.make_nt('master_node.medium', 'JT+NN', 1234, 2345)
-    #     data_nt_master = self._post_object(self.url_nt, nt_body, 202)
-    #
-    #     nt_body = self.make_nt('worker_node.medium', 'TT+DN', 1234, 2345)
-    #     data_nt_worker = self._post_object(self.url_nt, nt_body, 202)
-    #
-    #     cluster_body = self.make_cluster_body(
-    #         'QA-cluster', 'master_node.medium', 'worker_node.medium', 3)
-    #     get_cluster_body = self._get_body_cluster(
-    #         'QA-cluster', 'master_node.medium', 'worker_node.medium', 3)
-    #     self._crud_object(cluster_body, get_cluster_body,
-    #                       self.url_cluster)
-    #
-    #     self.delete_node_template(data_nt_master)
-    #     self.delete_node_template(data_nt_worker)
+    def test_crud_operation_for_cluster(self):
+        nt_body = self.make_nt('master_node.medium', 'JT+NN', 1234, 2345)
+        data_nt_master = self._post_object(self.url_nt, nt_body, 202)
+
+        nt_body = self.make_nt('worker_node.medium', 'TT+DN', 1234, 2345)
+        data_nt_worker = self._post_object(self.url_nt, nt_body, 202)
+
+        try:
+            cluster_body = self.make_cluster_body(
+                'QA-ylobankov', 'master_node.medium', 'worker_node.medium', 3)
+            get_cluster_body = self._get_body_cluster(
+                'QA-ylobankov', 'master_node.medium', 'worker_node.medium', 3)
+
+            self._crud_object(cluster_body, get_cluster_body, self.url_cluster)
+
+        finally:
+            self.delete_node_template(data_nt_master)
+            self.delete_node_template(data_nt_worker)
+
+        # nt_body = self.make_nt('master_node.medium', 'JT+NN', 1234, 2345)
+        # data_nt_master = self._post_object(self.url_nt, nt_body, 202)
+        #
+        # nt_body = self.make_nt('worker_node.medium', 'TT+DN', 1234, 2345)
+        # data_nt_worker = self._post_object(self.url_nt, nt_body, 202)
+        #
+        # cluster_body = self.make_cluster_body(
+        #     'QA-ylobankov', 'master_node.medium', 'worker_node.medium', 2)
+        #
+        # self._post_object(self.url_cluster, cluster_body, 202)
+        #
+        # # eventlet.sleep(300)
+        #
+        # self.delete_node_template(data_nt_master)
+        # self.delete_node_template(data_nt_worker)
--- savanna/tests/integration/test_hadoop.py
+++ savanna/tests/integration/test_hadoop.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2013 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from os import getcwd
 import paramiko
 from re import search
@@ -35,6 +50,7 @@ def _execute_transfer_on_node(host, locfile, nodefile):
         transport.connect(username='root', password='swordfish')
         sftp = paramiko.SFTPClient.from_transport(transport)
         sftp.put(locfile, nodefile)
+
     finally:
         sftp.close()
         transport.close()
@@ -46,6 +62,7 @@ def _execute_transfer_from_node(host, nodefile, localfile):
         transport.connect(username='root', password='swordfish')
         sftp = paramiko.SFTPClient.from_transport(transport)
         sftp.get(nodefile, localfile)
+
     finally:
         sftp.close()
         transport.close()
@@ -63,6 +80,7 @@ class TestForHadoop(ValidationTestCase):
         cluster_body = self.make_cluster_body(
             cluster_name, nt_name_master, nt_name_worker, number_workers)
         data = self._post_object(self.url_cluster, cluster_body, 202)
+
         try:
             data = data['cluster']
             object_id = data.pop(u'id')
@@ -73,10 +91,12 @@ class TestForHadoop(ValidationTestCase):
             del get_data[u'id']
             self._response_cluster(
                 get_body, get_data, self.url_cl_wj, object_id)
+
             get_data = self._get_object(self.url_cl_wj, object_id, 200, True)
             get_data = get_data['cluster']
             namenode = get_data[u'service_urls'][u'namenode']
             jobtracker = get_data[u'service_urls'][u'jobtracker']
+
             p = '(?:http.*://)?(?P<host>[^:/ ]+).?(?P<port>[0-9]*).*'
             m = search(p, namenode)
             t = search(p, jobtracker)
@@ -84,24 +104,30 @@ class TestForHadoop(ValidationTestCase):
             namenode_port = m.group('port')
             jobtracker_ip = t.group('host')
             jobtracker_port = t.group('port')
+
             Telnet(str(namenode_ip), str(namenode_port))
             Telnet(str(jobtracker_ip), str(jobtracker_port))
+
             this_dir = getcwd()
             _execute_transfer_on_node(
                 str(namenode_ip), '%s/integration/script.sh' % this_dir,
                 '/script.sh')
+
             try:
                 self.assertEquals(
                     _execute_command_on_node(
                         namenode_ip,
                         "cd .. && chmod 777 script.sh && ./script.sh"), 0)
+
             except Exception as e:
                 _execute_transfer_from_node(
                     namenode_ip, '/outputTestMapReduce/log.txt',
                     '%s' % this_dir)
                 self.fail("run script is failure" + e.message)
+
         except Exception as e:
             self.fail("failure:" + e.message)
+
         finally:
             self._del_object(self.url_cl_wj, object_id, 204)
 
@@ -112,12 +138,14 @@ class TestForHadoop(ValidationTestCase):
         data_nt_worker = self._post_object(
             self.url_nt, self.make_nt('worker_node.medium', 'TT+DN',
                                       1234, 2345), 202)
+
         try:
             self._hadoop_testing(
-                'qa-hadoop-testing',
-                'master_node.medium', 'worker_node.medium', 2)
+                'QA-hadoop', 'master_node.medium', 'worker_node.medium', 2)
+
         except Exception as e:
             self.fail("failure:" + str(e))
+
         finally:
             self.delete_node_template(data_nt_master)
             self.delete_node_template(data_nt_worker)
