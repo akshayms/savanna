239eb5fc5924d00ad40f20d7bd6c215c8c3fc0c0
Vadim Rovachev <vrovachev@mirantis.com>
2013-07-23 13:55:47 +0400

    add changes for ui tests

--- savanna/tests/uitests/positive_tests/test_create_cluster.py
+++ savanna/tests/uitests/positive_tests/test_create_cluster.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from savanna.tests.ui import base
+from savanna.tests.uitests import base
 
 
 class UIClusterCreate(base.UITestCase):
